6b3d9815c421ba5838919982bb82117bcbb40e3d
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-26 21:55:02 +0300

    inital-solution

--- server.js
+++ server.js
@@ -31,47 +31,92 @@ server.get('/test', (req, res) => {
 
 })
 
-// step 4
-// http://localhost:3001/weather?city_name=Amman?lat=31.95?lon=35.91
-server.get('/weather', (req, res) => {
+server.get('/weather', weatherHandler)
+
+function weatherHandler(req, res) {
     let weatherCityData = req.query.city_name;
-    // let weatherLatData = req.query.lat;
-    // let weatherLonData = req.query.lon;
-    //let cityData;
+    let key = process.env.UNSPLASH_KEY;
+    let url = `https://api.weatherbit.io/v2.0/forcast/daily?query=${weatherCityData}?key=${key}`
 
-    //create function to  get data weather from 
-    //&& city.lat == weatherLatData && city.lon == weatherLonData
     let weatherCity = weatherData.find(city => {
-        if (city.city_name.toLowerCase()==weatherCityData.toLowerCase()) {
-            //cityData = new Forecast(city)
+        if (city.city_name.toLowerCase() == weatherCityData.toLowerCase()) {
             return city
-
+    
         }
-
+    
     });
-    //console.log(weatherCityData)
+
+    axios
+        .get(url)
+        .then(result => {
+            console.log('inside promise')
+
+            let foccasrtDataArr = weatherCity.data.map(item => {
+                return new Forecast(item)
+            })
+            res.send(foccasrtDataArr)
+        })
+        .catch(err => {
+            res.status(500).send(`error in getting the photo data ==> ${err}`);
+        })
+
+    console.log('after aaxios');
+}
+
+// ....
+ server.get('/moive', moiveHandler)
+
+ function moiveHandler(req, res) {
+  let movie = req.query.query;
+    let keyM = process.env.MOVIE_API;
+    let urlM = `https://api.themoviedb.org/3/search/movie?api_key=${keyM}&query=${movie}`
+
+//     let weatherCity = weatherData.find(city => {
+//         if (city.city_name.toLowerCase() == weatherCityData.toLowerCase()) {
+//             return city
+    
+//         }
+    
+//     });
+
+//     axios
+//         .get(url)
+//         .then(result => {
+//             console.log('inside promise')
+
+//             let foccasrtDataArr = weatherCity.data.map(item => {
+//                 return new Forecast(item)
+//             })
+//             res.send(foccasrtDataArr)
+//         })
+//         .catch(err => {
+//             res.status(500).send(`error in getting the photo data ==> ${err}`);
+//         })
+
+//     console.log('after aaxios');
+// }
 
 
-
-let foccasrtDataArr= weatherCity.data.map(item=>{
-    return new Forecast(item)
-})
-res.send(foccasrtDataArr)
-})
-
-//here example what we want in class 
-// "description": "Low of 17.5, high of 29.9 with few clouds",
-//     "date": "2021-04-01
 class Forecast {
     constructor(arr) {
 
         this.date = arr.valid_date
-        this.descrption = arr.weather.description;       
+        this.descrption = arr.weather.description;
 
 
     }
 }
 
+class Movie {
+    constructor(movieCons) {
+      this.title = movieCons.title;
+      this.overview = movieCons.overview;
+      this.popularity = movieCons.popularity;
+      this.vote_average=movieCons.vote_average;
+      this.release_date=movieCons.release_date;
+      this.poster_path=movieCons.poster_path
+    }
+  }
 
 // // step 5
 // if we search about something wrong 
