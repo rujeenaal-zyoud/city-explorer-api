bbab7269ded1a898416664eaa30e2bc0248c8d65
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-30 14:18:15 +0300

    URL

--- server.js
+++ server.js
@@ -19,11 +19,52 @@ server.get('/', (request, response) => {
 })
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000 ;
+let WinMemory={};
 
-server.get('/weather', weatherHandler);
+server.get('/weather', function weatherHandler(req, res) {
+    let weatherCityData = req.query.city_name;
+    let keyW = process.env.WEATHER_API;
+
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${weatherCityData}&key=${keyW}`
+// add memory for API request
+
+    if (WinMemory[weatherCityData] !== undefined) {
+        console.log('get the Weather data from the Memory')
+        res.send(WinMemory[weatherCityData])
+      } 
+      else {
+        console.log('get the Weather data from the API');
+        axios
+          .get(url)
+            .then(result => {
+                let foccasrtDataArr = result.data.data.map(item => new Forecast(item))              //
+                WinMemory[weatherCityData] = foccasrtDataArr;
+            res.status(200).send(WinMemory[weatherCityData]);
+          })
+          .catch(err => {
+            console.err('error', err);
+            res.status(500).send('error', err);
+          })
+          
+          
+
+}
+});
+
+class Forecast {
+    constructor(item) {
+
+        this.date = item.valid_date
+        this.descrption = item.weather.description;
+
+
+    }
+}
 
 server.get('/movies', movieHandler);
+
+
 
 
 
